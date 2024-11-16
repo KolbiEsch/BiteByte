2d4b4254e4a3efab42bc35c30dadf1ce231e3d09
Kolbi <kolbiesch10@gmail.com>
2024-11-16 13:16:09 -0500

    make profile pane dynamic

--- BiteByte/src/com/cmu/ProfilePane.java
+++ BiteByte/src/com/cmu/ProfilePane.java
@@ -1,14 +1,56 @@
 package com.cmu;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 public class ProfilePane {
 	
+	private static Button accountBtn;
+	private static Button ordersBtn;
+	
 	public Pane getProfilePane() {
-		GridPane grid = new GridPane();
+		BorderPane pane = new BorderPane();
+		
+		Pane profileNav = getNav();
+		Pane accountPane = getAccountPane();
+		
+		pane.setLeft(profileNav);
+		pane.setCenter(accountPane);
+		
+		GridPane ordersGrid = new GridPane();
+		
+		accountBtn.setOnAction(e -> {
+			pane.setCenter(accountPane);
+		});
+		
+		ordersBtn.setOnAction(e -> {
+			pane.setCenter(ordersGrid);
+		});
+		
+		return pane;
+	}
+	
+	private static Pane getNav() {
+		VBox profileNav = new VBox();
+		
+		accountBtn = new Button("Account");
+		ordersBtn = new Button("Orders");
+		
+		profileNav.getChildren().addAll(accountBtn, ordersBtn);
+		profileNav.setPadding(new Insets(15));
+		profileNav.setMargin(ordersBtn, new Insets(10, 0, 0, 0));
+		
+		return profileNav;
+	}
+	
+	private static Pane getAccountPane() {
+		GridPane accountGrid = new GridPane();
 		
 		Label line1Lbl = new Label("Line 1:");
 		Label line2Lbl = new Label("Line 2:");
@@ -22,21 +64,20 @@ public class ProfilePane {
 		TextField stateField = new TextField();
 		TextField ZIPField = new TextField();
 		
-		grid.add(line1Lbl, 0, 0);
-		grid.add(line1Field, 1, 0);
+		accountGrid.add(line1Lbl, 0, 0);
+		accountGrid.add(line1Field, 1, 0);
 		
-		grid.add(line2Lbl, 0, 1);
-		grid.add(line2Field, 1, 1);
-		grid.add(cityLbl, 0, 2);
-		grid.add(cityField, 1, 2);
-		grid.add(stateLbl, 0, 3);
-		grid.add(stateField, 1, 3);
-		grid.add(ZIPLbl, 0, 4);
-		grid.add(ZIPField, 1, 4);
+		accountGrid.add(line2Lbl, 0, 1);
+		accountGrid.add(line2Field, 1, 1);
+		accountGrid.add(cityLbl, 0, 2);
+		accountGrid.add(cityField, 1, 2);
+		accountGrid.add(stateLbl, 0, 3);
+		accountGrid.add(stateField, 1, 3);
+		accountGrid.add(ZIPLbl, 0, 4);
+		accountGrid.add(ZIPField, 1, 4);
 		
-		grid.setStyle("-fx-border-color: orange");
+		accountGrid.setStyle("-fx-border-color: orange");
 		
-		return grid;
+		return accountGrid;
 	}
-	
 }
