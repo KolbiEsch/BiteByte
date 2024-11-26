25de3862363469c671702adda8a4948530e12be6
Kolbi <kolbiesch10@gmail.com>
2024-11-25 19:31:40 -0500

    update driver to take in user

--- BiteByte/src/com/cmu/DeliveryDriver.java
+++ BiteByte/src/com/cmu/DeliveryDriver.java
@@ -1,5 +1,6 @@
 package com.cmu;
 
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
@@ -7,10 +8,28 @@ import java.util.List;
 public class DeliveryDriver extends User {
 
 	public HashMap<String, Order> ongoingOrders;
-	public Deque<Order> orderQueue;
 	public List<Order> completedOrders;
 	
 	public DeliveryDriver(String name, String email, String password) {
 		super(name, email, password);
+		this.ongoingOrders = new HashMap<>();
+		this.completedOrders = new ArrayList<>();
+		
+	}
+
+	public HashMap<String, Order> getOngoingOrders() {
+		return ongoingOrders;
+	}
+
+	public void addOngoingOrder(String orderID, Order order) {
+		this.ongoingOrders.put(orderID, order);
+	}
+
+	public List<Order> getCompletedOrders() {
+		return completedOrders;
+	}
+
+	public void addCompletedOrder(Order order) {
+		this.completedOrders.add(order);
 	}
 }
