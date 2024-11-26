e1d0c8afdad94facb7f1e3b7c0cd93a024036936
Honey Badger <canine148@gmail.com>
2024-11-25 22:50:08 -0500

    Update DeliveryDriverPane.java
    Added more structure

--- BiteByte/src/com/cmu/DeliveryDriverPane.java
+++ BiteByte/src/com/cmu/DeliveryDriverPane.java
@@ -1,12 +1,15 @@
 package com.cmu;
 
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
 public class DeliveryDriverPane implements BasePane{
 
@@ -21,13 +24,17 @@ public class DeliveryDriverPane implements BasePane{
 	{
 		BorderPane borderPane = new BorderPane();
 		
-		Label driverInformation = new Label("Driver: " + driver.getName() + " Email: " + driver.getEmail());
+		Label driverName = new Label("Driver: " + driver.getName());
+		HBox driverInfo = new HBox();
+		driverInfo.setAlignment(Pos.TOP_CENTER);
+		driverInfo.getChildren().add(driverName);
 		
 		VBox ordersBox = new VBox(10);
 		ordersBox.getChildren().addAll(getOngoingOrders(), getCompletedOrders());
+		ordersBox.setAlignment(Pos.CENTER);
 		
 		borderPane.setCenter(ordersBox);
-		borderPane.setTop(driverInformation);
+		borderPane.setTop(driverInfo);
 		
 	
 		return borderPane;
@@ -36,9 +43,25 @@ public class DeliveryDriverPane implements BasePane{
 	public Pane getOngoingOrders()
 	{
 		
-		ListView<String> ongoingOrders = new ListView<>();		
+		ListView<String> ongoingOrders = new ListView<>();	
+//		ongoingOrders.setCellFactory(e -> new ListCell<String>{
+//	        @Override
+//	        protected void updateItem(String item, boolean empty) {
+//	            super.updateItem(item, empty);
+//	            if (!empty && item != null) { // Ensure that the item is not null and the cell is not empty
+//	                setText(item);
+//	                setFont(Font.font(12)); // Setting the font size to 12
+//	            } else {
+//	                setText(null); // Clear the text to ensure cells are properly cleared
+//	            }
+//	        }
+//	    });
+
+				
+		
 		
 		Button btnOngoing = new Button("Refresh Current Orders");
+		btnOngoing.setMaxSize(275, 30);
 		btnOngoing.setOnAction(e -> 
 		{
 			ongoingOrders.getItems().clear();
@@ -47,13 +70,17 @@ public class DeliveryDriverPane implements BasePane{
 		    for (String orderId : driver.ongoingOrders.keySet()) 
 		    {
 		        Order order = driver.ongoingOrders.get(orderId);
-		        ongoingOrders.getItems().add(orderId + ": " + order.getStatus());
+		        ongoingOrders.getItems().add("Order ID: " + orderId + "Customer: " + order.getCustomer().getName() + " Status:" + order.getStatus());
 		    }
 		});
 		
 		
+		
 		VBox vBox = new VBox();
+		vBox.setMaxWidth(275);
+		vBox.setMaxHeight(150);
 		vBox.getChildren().addAll(btnOngoing, ongoingOrders);
+		
 		
 		return vBox;
 	
@@ -65,6 +92,7 @@ public class DeliveryDriverPane implements BasePane{
 		ListView<String> completedOrders = new ListView<>();
 		
 		Button btnCompleted = new Button("Refresh Completed Orders");
+		btnCompleted.setMaxSize(275, 30);
 		btnCompleted.setOnAction(e -> 
 		{
 			completedOrders.getItems().clear();
@@ -78,9 +106,17 @@ public class DeliveryDriverPane implements BasePane{
 		});
 		
 		VBox vBox = new VBox();
+		vBox.setMaxWidth(275);
+		vBox.setMaxHeight(150);
 		vBox.getChildren().addAll(btnCompleted, completedOrders);
 		
 		return vBox;
+		
+	}
+	
+	public Pane getDeliveryInformation(){
+		
+		Label customerDetails = new Label();
 		
 	}
 
