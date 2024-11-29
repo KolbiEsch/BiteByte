19f579afbd64fd72969850a5f729ba58e995f848
OdilD <dilmurodov.odil2901@gmail.com>
2024-11-29 09:12:28 -0600

    Update NavbarPane.java

--- BiteByte/src/com/cmu/NavbarPane.java
+++ BiteByte/src/com/cmu/NavbarPane.java
@@ -8,30 +8,33 @@ import javafx.scene.layout.Pane;
 
 public class NavbarPane implements BasePane {
 
-	BorderPane mainPane;
-	private ProfilePane profilePane;
-	
-	public NavbarPane(BorderPane mainPane) {
-		this.mainPane = mainPane;
-		this.profilePane = new ProfilePane();
-	}
-	
-	public Pane getPane() {
-		HBox navbar = new HBox();
-		
-		Button profileBtn = new Button("Profile");
-		navbar.setMargin(profileBtn, new Insets(0, 10, 0, 0));
-		Button restaurantsBtn = new Button("Restaurants");
-		
-		navbar.setPadding(new Insets(15));
-		navbar.getChildren().addAll(profileBtn, restaurantsBtn);
-		
-		// Example of BorderPane nav relationship.
-		profileBtn.setOnAction(e -> {
-			mainPane.setCenter(profilePane.getPane());
-		});
-		
-		return navbar;
-	}
-	
+    private BorderPane mainPane;
+    private ProfilePane profilePane;
+    private RestaurantMenuPane restaurantMenuPane;
+
+    public NavbarPane(BorderPane mainPane) {
+        this.mainPane = mainPane;
+        this.profilePane = new ProfilePane();
+        this.restaurantMenuPane = new RestaurantMenuPane(mainPane); // Initialize RestaurantMenuPane
+    }
+
+    public Pane getPane() {
+        HBox navbar = new HBox();
+
+        Button profileBtn = new Button("Profile");
+        navbar.setMargin(profileBtn, new Insets(0, 10, 0, 0));
+        Button restaurantsBtn = new Button("Restaurants");
+
+        navbar.setPadding(new Insets(15));
+        navbar.getChildren().addAll(profileBtn, restaurantsBtn);
+
+        // Set action for profileBtn
+        profileBtn.setOnAction(e -> mainPane.setCenter(profilePane.getPane()));
+
+        // Set action for restaurantsBtn
+        restaurantsBtn.setOnAction(e -> mainPane.setCenter(restaurantMenuPane.getPane()));
+
+        return navbar;
+    }
 }
+
